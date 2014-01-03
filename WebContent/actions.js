7889a693d54b7849d490b9e340c3e9fc1af538fa
Kristof Buts <kristof.buts@gmail.com>
2014-01-03 23:55:55 +0100

    formatting js file

--- WebContent/actions.js
+++ WebContent/actions.js
@@ -14,14 +14,14 @@ var SOUNDS = (function() {
 	playSound = function(srcGeluid) {
 		var sound = new Audio();
 		sound.src = srcGeluid;
-	    sound.play();
+		sound.play();
 		logForDebugging('played ' + srcGeluid + '!');
 	};
-	
+
 	return {
-		playSoundRightAnswer: playSoundRightAnswer,
-		playSoundWrongAnswer: playSoundWrongAnswer,
-		playSound: playSound
+		playSoundRightAnswer : playSoundRightAnswer,
+		playSoundWrongAnswer : playSoundWrongAnswer,
+		playSound : playSound
 	};
 })();
 
@@ -30,25 +30,26 @@ createExercise = function(exerciseName) {
 	var question = 'undefined';
 	var rightAnswer = 0;
 	var selectedAnswer = 1;
-	
+
 	$.getJSON(name + '/exercise.json', function(data) {
 		question = data.question;
 		rightAnswer = data.rightAnswer;
 		setUp();
-	}, function() {});
-	
+	}, function() {
+	});
+
 	isRightAnswerSelected = function() {
-	  return rightAnswer == selectedAnswer;
+		return rightAnswer == selectedAnswer;
 	};
-	
+
 	playQuestion = function() {
 		SOUNDS.playSound(getQuestionSound());
 	};
-	
+
 	getQuestionSound = function() {
 		return name + '/question.mp3';
 	};
-	
+
 	setUp = function() {
 		$('#title').html(question);
 		setUpAnswerImageFor(1);
@@ -56,45 +57,45 @@ createExercise = function(exerciseName) {
 		setUpAnswerImageFor(3);
 		setUpAnswerImageFor(4);
 	};
-	
+
 	setUpAnswerImageFor = function(answerNumber) {
 		$(answerIdFor(answerNumber)).attr('src', answerImageFor(answerNumber));
 	};
-	
+
 	answerImageFor = function(answerNumber) {
 		return name + '/' + answerNumber + '.png';
 	};
-	
+
 	selectAnswer = function(newSelectedAnswer) {
 		selectedAnswer = newSelectedAnswer;
 	};
-	
+
 	return {
-		isRightAnswerSelected: isRightAnswerSelected,
-		selectAnswer: selectAnswer,
-		playQuestion: playQuestion
+		isRightAnswerSelected : isRightAnswerSelected,
+		selectAnswer : selectAnswer,
+		playQuestion : playQuestion
 	};
 };
 
 var APP = (function() {
 	var currentExercise = null;
-	
+
 	setUp = function() {
-      logForDebugging('setting up app');
-      setUpPossibleAnswers();
-	  setUpExercise('exercise1');
+		logForDebugging('setting up app');
+		setUpPossibleAnswers();
+		setUpExercise('exercise1');
 	};
-	
+
 	setUpExercise = function(name) {
 		currentExercise = createExercise(name);
 		APP.selectAnswer(1);
 		playQuestionSound();
 	};
-	
+
 	playQuestionSound = function() {
-	  currentExercise.playQuestion();
+		currentExercise.playQuestion();
 	};
-	
+
 	setUpPossibleAnswers = function() {
 		attachListenerForAnswerNumber(1);
 		attachListenerForAnswerNumber(2);
@@ -103,7 +104,9 @@ var APP = (function() {
 	};
 
 	attachListenerForAnswerNumber = function(answerNumber) {
-		$(answerIdFor(answerNumber)).click(function() { APP.selectAnswer(answerNumber); });
+		$(answerIdFor(answerNumber)).click(function() {
+			APP.selectAnswer(answerNumber);
+		});
 	};
 
 	selectAnswer = function(answerNumber) {
@@ -132,46 +135,46 @@ var APP = (function() {
 			wrongAnswerWasSelected();
 		}
 	};
-	
+
 	showValidateButton = function() {
 		hideAllButtons();
 		$('#validateButton').show();
 	};
-	
+
 	rightAnswerWasSelected = function() {
 		hideAllButtons();
 		$('#rightButton').show();
 		SOUNDS.playSoundRightAnswer();
 	};
-	
+
 	wrongAnswerWasSelected = function() {
 		hideAllButtons();
 		$('#wrongButton').show();
 		SOUNDS.playSoundWrongAnswer();
 	};
-	
+
 	hideAllButtons = function() {
 		$('#rightButton').hide();
 		$('#wrongButton').hide();
 		$('#validateButton').hide();
 	};
-	
+
 	next = function() {
-		  setUpExercise('exercise2');
+		setUpExercise('exercise2');
 	};
-	
+
 	previous = function() {
-		  setUpExercise('exercise1');
+		setUpExercise('exercise1');
 	};
-	
+
 	return {
-		currentExercise: currentExercise,
-		setUp: setUp,
-		selectAnswer: selectAnswer,
-		checkAnswer: checkAnswer,
-		playQuestionSound: playQuestionSound,
-		next: next,
-		previous: previous
+		currentExercise : currentExercise,
+		setUp : setUp,
+		selectAnswer : selectAnswer,
+		checkAnswer : checkAnswer,
+		playQuestionSound : playQuestionSound,
+		next : next,
+		previous : previous
 	};
 })();
 
@@ -180,12 +183,12 @@ answerIdFor = function(answerNumber) {
 };
 
 initializeApp = function() {
-  logForDebugging('starting app...');
-  $('#validateButton').click(APP.checkAnswer);
-  $('#replayQuestion').click(APP.playQuestionSound);
-  $('#previousLink').click(APP.previous);
-  $('#nextLink').click(APP.next);
-  APP.setUp();
+	logForDebugging('starting app...');
+	$('#validateButton').click(APP.checkAnswer);
+	$('#replayQuestion').click(APP.playQuestionSound);
+	$('#previousLink').click(APP.previous);
+	$('#nextLink').click(APP.next);
+	APP.setUp();
 };
 
 $(initializeApp);
